b31e432c797cd4ff1893d13ab414c681bbc72f3a
Amani Chennu <112126839+amanichennu201@users.noreply.github.com>
2022-11-03 00:16:00 -0500

    added changes

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var BagRouter = require('./routes/Bag');
 var gridBuildRouter = require('./routes/gridbuild');
+
+var selectorRouter = require('./routes/selector');
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Bag',BagRouter);
 app.use('/gridbuild',gridBuildRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
